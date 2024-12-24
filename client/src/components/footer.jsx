8aab73313980a2eb9a30d82ed38f0b336e405380
Lielina19 <lielinaakele@gmail.com>
2024-12-24 20:33:31 +0300

    Added 4 cards

--- client/src/components/footer.jsx
+++ client/src/components/footer.jsx
@@ -10,8 +10,31 @@ function Footer() {
         {/* Logo and Description */}
         <div className="footer-logo">
           <h3>Ministry of Innovation and Technology</h3>
+
           <p>Driving innovation and technology for a better future.</p>
         </div>
+
+      </div>
+      {/* New section for divided windows */}
+      <div className="gridContainer">
+        <div className="window">
+          <h2>General</h2>
+          <p>This website is the planning and Monitoring website of Ministry of Innovation and Technology. The plans are divided into a general Goal which will include KPAs and KPIs of the specific sectors found in the Ministry. And the KPIs are recorded in percent or in number, allowing comparison of planned versus achieved values.</p>
+        </div>
+        <div className="window">
+          <h2>Goals</h2>
+          <p>Here the Goals of the Ministry will emerge once the minister has approved the plans. Currently, since no plan is approved, this section is empty.</p>
+        </div>
+        <div className="window">
+          <h2>KPA</h2>
+          <p>Ministry of Innovation and Technology works on its plan based on the Key Performance Area (KPA), which are the critical areas of an organization’s performance that are crucial for its success. KPAs refer to the general areas of outcomes within the business, while KPIs are used to measure the success of these areas.</p>
+        </div>
+        <div className="window">
+          <h2>KPI</h2>
+          <p>The KPIs are measurable values that demonstrate how effectively an organization is achieving its key business objectives. They are used to evaluate the success of an organization, employee, or project in meeting specific goals. In our institute, you will see the KPAs in number or percent.</p>
+        </div>
+      </div>
+
 
         {/* Navigation Links */}
         <div className="footer-links">
@@ -48,7 +71,6 @@ function Footer() {
             </a>
           </div>
         </div>
-      </div>
 
       {/* Copyright */}
       <div className="footer-bottom">
